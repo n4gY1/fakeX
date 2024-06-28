bd0daaf6e8323a1b66f60c29bad1139adfa332c4
nagy- <hybase@outlook.hu>
2024-06-28 19:40:57 +0200

    main height

--- fakeX/settings.py
+++ fakeX/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2ct#x6nchs#dzf%^q4(5q@^^x1c=fopvb41x2v(zpl_9k@v#j=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
